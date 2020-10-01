ed2cfedd8716a86e6631dd341aaa096ed16fa921
Sebastian Springer <info@sebastian-springer.com>
2020-10-01 14:35:33 +0200

    custom hook

--- src/List/List.tsx
+++ src/List/List.tsx
@@ -1,47 +1,10 @@
 import * as React from 'react';
-import { useEffect, useState } from 'react';
-import Log from '../shared/Log';
 import ListItem from './ListItem';
 import useLogs from './useLogs';
 
-// const initialLogData: Log[] = [{
-//   id: 1,
-//   from: '09:00',
-//   until: '10:00',
-//   title: 'work',
-//   duration: 60
-// }, {
-//   id: 2,
-//   from: '10:00',
-//   until: '12:00',
-//   title: 'work even more',
-//   duration: 120
-// }];
-
 const List: React.FC = () => {
-  const title = useLogs();
-  const [logs, setLogs] = useState<Log[]>([]);
-
-  useEffect(() => {
-    (async () => {
-      const response = await fetch('http://localhost:3001/logs');
-      const data = await response.json();
-      setLogs(data);
-    })();
-  },[]);
-
-  async function handleDelete(item: Log): Promise<void> {
-    const response = await fetch(`http://localhost:3001/logs/${item.id}`, {method: 'DELETE'});
-
-    if (response.status === 200) {
-      setLogs((prevLogs) => {
-        return prevLogs.filter((prevLog) => prevLog.id !== item.id);
-      });
-    }
-  }
-
+  const {logs, handleDelete} = useLogs();
   return <>
-    <h1>{title}</h1>
     <table>
       <thead>
         <tr>
--- src/List/useLogs.ts
+++ src/List/useLogs.ts
@@ -1,13 +1,29 @@
 import { useEffect, useState } from "react";
+import Log from "../shared/Log";
 
 export default function useLogs() {
-  const [state, setState] = useState('Timetracking Applikation');
+  const [logs, setLogs] = useState<Log[]>([]);
 
   useEffect(() => {
-    setTimeout(() => {
-      setState((prevState) => `Fancy ${prevState}`);
-    }, 2000); 
-  }, []);
+    (async () => {
+      const response = await fetch('http://localhost:3001/logs');
+      const data = await response.json();
+      setLogs(data);
+    })();
+  },[]);
 
-  return [state, setState];
+  async function handleDelete(item: Log): Promise<void> {
+    const response = await fetch(`http://localhost:3001/logs/${item.id}`, {method: 'DELETE'});
+
+    if (response.status === 200) {
+      setLogs((prevLogs) => {
+        return prevLogs.filter((prevLog) => prevLog.id !== item.id);
+      });
+    }
+  }
+
+  return {
+    logs,
+    handleDelete
+  }
 }
