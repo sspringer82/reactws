47ba7d4e783875edbc5218ce64ba14285f02f61d
Sebastian Springer <info@sebastian-springer.com>
2020-10-01 11:22:06 +0200

    list component

--- src/List/List.tsx
+++ src/List/List.tsx
@@ -1,7 +1,44 @@
 import * as React from 'react';
 
+interface Item {
+  id: number;
+  title: string;
+}
+
 const List: React.FC = () => {
-  return <div>List component works</div>;
+
+  const [state, setState] = React.useState<string>('Hallo Welt');
+  // const [state, setState] = React.useState<Item[]>([]);
+
+  React.useEffect(() => {
+    setTimeout(() => {
+      // setState('Hallo React');
+      setState((prevState) => {
+        return 'Hallo React';
+      });
+    }, 5000);
+    return () => {} // handle unmount
+  }, []); // [] 1x initial ausführen
+
+  const logs = [{
+    id: 1,
+    from: '09:00',
+    until: '10:00',
+    title: 'work',
+    duration: 60
+  }, {
+    id: 2,
+    from: '10:00',
+    until: '12:00',
+    title: 'work even more',
+    duration: 120
+  }];
+
+  
+
+  return <div>{state}</div>;
+
+
 };
 
 export default List;
